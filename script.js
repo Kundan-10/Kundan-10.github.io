9bdf430ad1b654c5fc797f2531583136e5b2be7f
Kundan Wakudkar <101393406+Kundan-10@users.noreply.github.com>
2022-12-18 22:24:47 +0530

    commits remove

--- script.js
+++ script.js
@@ -33,20 +33,7 @@ $(document).ready(function () {
     $(".menu-btn i").toggleClass("active");
   });
 
-  // typing text animation script
-  // var typed = new Typed(".typing", {
-  //   strings: ["Back-End Developer", "Dreamer"],
-  //   typeSpeed: 100,
-  //   backSpeed: 60,
-  //   loop: true,
-  // });
 
-  // var typed = new Typed(".typing-2", {
-  //   strings: ["Back-End Developer", "Dreamer"],
-  //   typeSpeed: 100,
-  //   backSpeed: 60,
-  //   loop: true,
-  // });
 
   // owl carousel script
   $(".carousel").owlCarousel({
